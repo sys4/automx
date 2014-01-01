fa7465e56219b0343d8bfbec5eeb7ff640852ee5
Christian Roessner <c@roessner-network-solutions.com>
2014-01-01 20:19:26 +0100

    [RELEASE] Releasing bug fixes

--- src/automx/__init__.py
+++ src/automx/__init__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 __author__ = "Christian Roessner, Patrick Ben Koetter"
 __copyright__ = "Copyright (c) 2011-2013 [*] sys4 AG"
 
--- src/automx/view.py
+++ src/automx/view.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from plistlib import writePlistToString, readPlist
 
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 __author__ = "Christian Roessner, Patrick Ben Koetter"
 __copyright__ = "Copyright (c) 2011-2013 [*] sys4 AG"
 
